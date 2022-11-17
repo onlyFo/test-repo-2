48185762382e3e6851f284c3e8ab92bb41eacec5
Yaroslav <gondarevyaroslav@gmail,com>
2022-11-17 22:58:02 +0300

    Add minus function to Main class

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -5,8 +5,13 @@ fun main() {
     val y = 7
 
     print(sum(x,y))
+    print(minus(x,y))
 }
 
 fun sum(x: Int, y: Int): Int {
     return x + y
+}
+
+fun minus(x: Int, y: Int): Int {
+    return x - y
 }
